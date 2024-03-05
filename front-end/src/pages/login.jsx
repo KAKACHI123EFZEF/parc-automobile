2787f572a625dd7627bc2b629e7b23abbc923c6f
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2024-03-05 18:42:17 +0100

    message

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -23,6 +23,7 @@ export default function Login() {
                 </CardFooter>
             </Card>
             <p>iehierhgierhefzfzejo</p>
+            <p>iefiehrer</p>
         </div>
     );
 }
