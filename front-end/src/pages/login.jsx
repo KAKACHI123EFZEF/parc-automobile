b901a250e7a956fbde207fad8d605456161090a3
HichamElamrani07 <hichamlife07@gmail.com>
2024-03-05 18:58:07 +0100

    message

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -23,7 +23,9 @@ export default function Login() {
                 </CardFooter>
             </Card>
             <p>iehierhgierhefzfzejo</p>
+
             <p>zaki</p>
+            <p>hicham</p>
         </div>
     );
 }
