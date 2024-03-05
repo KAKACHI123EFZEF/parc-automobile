3cf8522c79d3d82b8f92553614337c5e64bfddf3
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2024-03-05 19:32:27 +0100

    ff

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,30 +1,9 @@
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "front-end/src/components/ui/card";
-
 export default function Login() {
     return (
         <div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
             <p>iehierhgierhefzfzejo</p>
         <p>hhelo assutya</p>
-        
+
         </div>
     );
 }
