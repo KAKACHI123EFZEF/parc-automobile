5fc3f598a2f5a5a1482d39b405df5d89431ffc5f
KAKACHI123EFZEF <125193387+KAKACHI123EFZEF@users.noreply.github.com>
2024-03-05 19:02:00 +0100

    test

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,27 +1,6 @@
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "front-end/src/components/ui/card";
-
 export default function Login() {
     return (
         <div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
             <p>iehierhgierhefzfzejo</p>
             <p>iefiehrer</p>
         </div>
